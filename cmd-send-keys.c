90c882ed3f55ee6a2546aa560a04a58ca7121cee
nicm <nicm>
2016-12-08 22:15:37 +0000

    Fix send-keys with UTF-8.

--- cmd-send-keys.c
+++ cmd-send-keys.c
@@ -62,7 +62,8 @@ cmd_send_keys_exec(struct cmd *self, struct cmd_q *cmdq)
 	struct window_pane	*wp = cmdq->state.tflag.wp;
 	struct session		*s = cmdq->state.tflag.s;
 	struct mouse_event	*m = &cmdq->item->mouse;
-	const u_char		*keystr;
+	struct utf8_data	*ud, *uc;
+	wchar_t			 wc;
 	int			 i, literal;
 	key_code		 key;
 
@@ -98,8 +99,12 @@ cmd_send_keys_exec(struct cmd *self, struct cmd_q *cmdq)
 				literal = 1;
 		}
 		if (literal) {
-			for (keystr = args->argv[i]; *keystr != '\0'; keystr++)
-				window_pane_key(wp, NULL, s, *keystr, NULL);
+			ud = utf8_fromcstr(args->argv[i]);
+			for (uc = ud; uc->size != 0; uc++) {
+				if (utf8_combine(uc, &wc) == UTF8_DONE)
+					window_pane_key(wp, NULL, s, wc, NULL);
+			}
+			free(ud);
 		}
 	}
 
