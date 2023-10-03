e90f0548f1e5673a71d1ad1627577b8f00399a63
WHR <msl0000023508@gmail.com>
2023-10-03 15:32:48 +0800

    Remove detection and compatibiltiy code for BSD-style unsigned integer types; remove getopt(3) checking from configure.ac

--- compat.h
+++ compat.h
@@ -35,13 +35,6 @@
 #define ECHOPRT 0
 #endif
 
-#ifndef HAVE_BSD_TYPES
-typedef uint8_t u_int8_t;
-typedef uint16_t u_int16_t;
-typedef uint32_t u_int32_t;
-typedef uint64_t u_int64_t;
-#endif
-
 #ifndef HAVE_PATHS_H
 #define	_PATH_BSHELL	"/bin/sh"
 #define	_PATH_TMP	"/tmp/"
